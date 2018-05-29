e46d010d2c8822d903ae3dce333f0f7732e8faee
podiumdesu <wwhmomo@gmail.com>
2018-05-29 23:21:10 +0800

    FIX: bind function

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -15,31 +15,30 @@ const contactIntro = {
 class Contact extends React.Component {
   constructor(props) {
     super(props)
-    this.handleDownload = this.handleDownload.bind(this)
+    // this.handleDownload = this.handleDownload.bind(this)
     // this.handleFileUpload = this.handleFileUpload.bind(this)
-  }
-  handleDownload() {
-    // alert('gg')
   }
   handleFileUpload(e) {
     const f = e.target.files[0]
-    const reader = new FileReader()
+    const formData = new FormData()
+    formData.append('file', f)
+    // const reader = new FileReader()
     // console.log(e.target.re)
-    const fileName = f.className
-    console.log(e)
+    // const fileName = f.className
+    // console.log(e)
     // console.log(`${fileName} is being read!`)
-    reader.onload = function (e) {
-      // console.log(e)
-      const data = e.target.result
-      const wb = XLSX.read(data, { type: 'binary' })
-      console.log(data)
-      processData(data)
-    }
-    reader.readAsText(f,'gb2312')
+    // reader.onload = function () {
+    //   // console.log(e)
+    //   const data = e.target.result
+    //   // const wb = XLSX.read(data, { type: 'binary' })
+    //   // console.log(data)
+    //   // processData(data)
+    // }
+    ajaxRequest('http://localhost:1234/map/console', 'POST', formData)
+    // reader.readAsText(f, 'gb2312')
   }
   clickToDownloadFile() {
     // alert('gg')
-    ajaxRequest('http://localhost:1234/map/console', 'POST', {data: 'ggg'})
   }
   render() {
     return (
@@ -47,11 +46,11 @@ class Contact extends React.Component {
         <PageIntro pageInfo={contactIntro} />
         <p>这里应该是一段很多很多的介绍嗷～</p>
         <p>请按照顺序填写表格</p>
-        <input type="file" id="inputCSV" accept=".csv, .xlsx" onChange={this.handleFileUpload} />
+        <input type="file" id="inputCSV" accept=".csv, .xlsx" onChange={this.handleFileUpload.bind(this)} />
         <p id="gg" onClick={this.handleFileUpload.bind(this)}>ggg</p>
         <input type="checkbox" id="fatherOrNot" />
         <label htmlFor="fatherOrNot">是否保存父母联系方式</label>
-        <div className={cls['clickToDownload']} onClick={this.clickToDownloadFile.bind(this)}>
+        <div className={cls.clickToDownload} onClick={this.clickToDownloadFile.bind(this)}>
           点击生成通讯录
         </div>
       </div>
